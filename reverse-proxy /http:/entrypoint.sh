76e721923d09a894d116215c181d28258294dfa2
alqysr828-arch <alqysr828@gmail.com>
2025-09-01 01:41:24 +0300

    Delete reverse-proxy /http: directory

--- reverse-proxy /http:/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-envsubst '
-  $BACKEND_HOST
-  $BACKEND_PORT
-  $FRONTEND_HOST
-  $FRONTEND_PORT
-' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
-
-nginx -g 'daemon off;'
