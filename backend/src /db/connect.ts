ba8e62fed6363439f30f056497ca628073a67231
alqysr828-arch <alqysr828@gmail.com>
2025-09-01 00:27:55 +0300

    Update connect.ts

--- backend/src /db/connect.ts
+++ backend/src /db/connect.ts
@@ -1,1 +1,9 @@
+import mongoose from "mongoose";
+import { env } from "../environments.js";
 
+export async function connectMongo() {
+  const auth = env.mongoUser && env.mongoPass ? ${env.mongoUser}:${env.mongoPass}@ : "";
+  const uri = mongodb://${auth}${env.mongoHost}/${env.mongoDbName}?authSource=admin;
+  await mongoose.connect(uri);
+  return mongoose.connection;
+}
