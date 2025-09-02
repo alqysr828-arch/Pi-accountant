7117fb5fac34b11efb6cac48efc30db055c1e58a
alqysr828-arch <alqysr828@gmail.com>
2025-09-03 01:41:30 +0300

    Update vite.config.js

--- frontend /vite.config.js
+++ frontend /vite.config.js
@@ -1,20 +1,18 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// إعدادات البناء + بروكسي لتجريب محلي
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // الرابط المحلي للBackend أثناء التطوير
+        target: 'http://localhost:8000',
         changeOrigin: true
       }
     }
   },
   build: {
-    outDir: 'dist',      // مجلد الإخراج لـ Vercel
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
